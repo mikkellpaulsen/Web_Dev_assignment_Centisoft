0c8d5ec3b700d003d64e39f7678af845a00121c0
mikkellpaulsen <mikkellpaulsen@gmail.com>
2018-02-07 22:50:24 +0100

    ;(
    ;(

---
+++ Web_Dev_Session1/Customers.js
@@ -0,0 +1,15 @@
+﻿var customer = new Array();
+customer[0] = "Stefan";
+customer[1] = "Casper";
+customer[2] = "Arne";
+customer[3] = "Tobias";
+customer[4] = "Mikkel";
+
+$(document).ready(function () {
+    var list = "";
+    for (i = 0; i < customer.length; i++) {
+        list += "<li>" + customer[i] + "</li>";
+    }
+    list += "<li>" + customer[i] + "</li>";
+    $("list-of-customers").append(list);
+});
