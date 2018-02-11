9f4c41dda957b7f1e7bfff34ad847907d3f5d29e
mikkellpaulsen <mikkellpaulsen@gmail.com>
2018-02-11 22:58:39 +0100

    :100: :fire: :fire:

--- Web_Dev_Session1/Customers.js
+++ Web_Dev_Session1/Customers.js
@@ -1,15 +1,16 @@
-﻿var customer = new Array();
+﻿const customer = new Array();
 customer[0] = "Stefan";
 customer[1] = "Casper";
 customer[2] = "Arne";
 customer[3] = "Tobias";
 customer[4] = "Mikkel";
 
-$(document).ready(function () {
-    var list = "";
-    for (i = 0; i < customer.length; i++) {
-        list += "<li>" + customer[i] + "</li>";
+window.onload = function () {
+    const ul = document.getElementById("list-of-customers");
+    for (let i = 0; i < customer.length; i++) {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(customer[i]));
+        li.setAttribute("class", "list-group-item");
+        ul.appendChild(li);
     }
-    list += "<li>" + customer[i] + "</li>";
-    $("list-of-customers").append(list);
-});
+};
